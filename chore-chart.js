84d36b173f7672e0c94507eb28022a372de29fff
Tommy A. Caruso <tommy.caruso2118@gmail.com>
2023-04-21 19:43:56 -0400

    Persist checkboxes

--- chore-chart.js
+++ chore-chart.js
@@ -4,13 +4,13 @@ const currentMonth = new Date().getMonth();
 const currentYear = new Date().getFullYear();
 const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
 
-function createChoreRow(day, date) {
+function createChoreRow(day, date, index) {
     const row = `
         <tr>
             <td>${day}</td>
             <td>${date}</td>
-            <td><input type="checkbox" class="walkDogs" data-earning="5" onchange="updateEarnings()"></td>
-            <td><input type="checkbox" class="feedDogs" data-earning="1.50" onchange="updateEarnings()"></td>
+            <td><input type="checkbox" class="walkDogs" data-earning="5" id="walkDogs-${index}" onchange="updateEarnings()"></td>
+            <td><input type="checkbox" class="feedDogs" data-earning="1.50" id="feedDogs-${index}" onchange="updateEarnings()"></td>
         </tr>
     `;
     return row;
@@ -21,9 +21,15 @@ function fillChoreTable() {
     for (let i = 1; i <= daysInMonth; i++) {
         const date = new Date(currentYear, currentMonth, i);
         const day = date.toLocaleString('en-us', { weekday: 'long' });
-        tableContent += createChoreRow(day, date.toLocaleDateString());
+        tableContent += createChoreRow(day, date.toLocaleDateString(), i);
     }
     choreTableBody.innerHTML = tableContent;
+}
+
+function saveCheckboxState(checkbox) {
+    const checkboxState = JSON.parse(localStorage.getItem('checkboxState')) || {};
+    checkboxState[checkbox.id] = checkbox.checked;
+    localStorage.setItem('checkboxState', JSON.stringify(checkboxState));
 }
 
 function updateEarnings() {
@@ -35,16 +41,28 @@ function updateEarnings() {
         if (checkbox.checked) {
             earnings += parseFloat(checkbox.dataset.earning);
         }
+        saveCheckboxState(checkbox);
     }
 
     for (const checkbox of feedDogsCheckboxes) {
         if (checkbox.checked) {
             earnings += parseFloat(checkbox.dataset.earning);
         }
+        saveCheckboxState(checkbox);
     }
 
     totalEarnings.textContent = earnings.toFixed(2);
     localStorage.setItem('choreChartEarnings', earnings.toFixed(2));
+}
+
+function loadCheckboxState() {
+    const checkboxState = JSON.parse(localStorage.getItem('checkboxState')) || {};
+    for (const checkboxId in checkboxState) {
+        const checkbox = document.getElementById(checkboxId);
+        if (checkbox) {
+            checkbox.checked = checkboxState[checkboxId];
+        }
+    }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -53,5 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
     if (storedEarnings) {
         totalEarnings.textContent = storedEarnings;
     }
+    loadCheckboxState();
+    updateEarnings();
 });
 
