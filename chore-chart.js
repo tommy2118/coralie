e55b5ac7c683e770de5216ac86654835209f63b8
Tommy A. Caruso <tommy.caruso2118@gmail.com>
2023-04-22 09:52:35 -0400

    Add Firebase

--- chore-chart.js
+++ chore-chart.js
@@ -4,74 +4,85 @@ const currentMonth = new Date().getMonth();
 const currentYear = new Date().getFullYear();
 const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
 
+// Replace with your own Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDSFoeEaMtWY5gxto43zycEqdV5z7LzfTw",
+    authDomain: "coralie-6eca5.firebaseapp.com",
+    projectId: "coralie-6eca5",
+    storageBucket: "coralie-6eca5.appspot.com",
+    messagingSenderId: "951783746067",
+    appId: "1:951783746067:web:3451fc0039c8376cedede6"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get a reference to the Firebase Realtime Database
+const database = firebase.database();
+
 function createChoreRow(day, date, index) {
-    const row = `
-        <tr>
-            <td>${day}</td>
-            <td>${date}</td>
-            <td><input type="checkbox" class="walkDogs" data-earning="5" id="walkDogs-${index}" onchange="updateEarnings()"></td>
-            <td><input type="checkbox" class="feedDogs" data-earning="1.50" id="feedDogs-${index}" onchange="updateEarnings()"></td>
-        </tr>
-    `;
-    return row;
+  const row = `
+    <tr>
+      <td>${day}</td>
+      <td>${date}</td>
+      <td><input type="checkbox" class="walkDogs" data-earning="5" id="walkDogs-${index}" onchange="updateEarnings()"></td>
+      <td><input type="checkbox" class="feedDogs" data-earning="1.50" id="feedDogs-${index}" onchange="updateEarnings()"></td>
+    </tr>
+  `;
+  return row;
 }
 
 function fillChoreTable() {
-    let tableContent = '';
-    for (let i = 1; i <= daysInMonth; i++) {
-        const date = new Date(currentYear, currentMonth, i);
-        const day = date.toLocaleString('en-us', { weekday: 'long' });
-        tableContent += createChoreRow(day, date.toLocaleDateString(), i);
-    }
-    choreTableBody.innerHTML = tableContent;
+  let tableContent = '';
+  for (let i = 1; i <= daysInMonth; i++) {
+    const date = new Date(currentYear, currentMonth, i);
+    const day = date.toLocaleString('en-us', { weekday: 'long' });
+    tableContent += createChoreRow(day, date.toLocaleDateString(), i);
+  }
+  choreTableBody.innerHTML = tableContent;
 }
 
 function saveCheckboxState(checkbox) {
-    const checkboxState = JSON.parse(localStorage.getItem('checkboxState')) || {};
-    checkboxState[checkbox.id] = checkbox.checked;
-    localStorage.setItem('checkboxState', JSON.stringify(checkboxState));
+  database.ref('checkboxState/' + checkbox.id).set(checkbox.checked);
 }
 
 function updateEarnings() {
-    const walkDogsCheckboxes = document.getElementsByClassName('walkDogs');
-    const feedDogsCheckboxes = document.getElementsByClassName('feedDogs');
-    let earnings = 0;
+  const walkDogsCheckboxes = document.getElementsByClassName('walkDogs');
+  const feedDogsCheckboxes = document.getElementsByClassName('feedDogs');
+  let earnings = 0;
 
-    for (const checkbox of walkDogsCheckboxes) {
-        if (checkbox.checked) {
-            earnings += parseFloat(checkbox.dataset.earning);
-        }
-        saveCheckboxState(checkbox);
+  for (const checkbox of walkDogsCheckboxes) {
+    if (checkbox.checked) {
+      earnings += parseFloat(checkbox.dataset.earning);
     }
+    saveCheckboxState(checkbox);
+  }
 
-    for (const checkbox of feedDogsCheckboxes) {
-        if (checkbox.checked) {
-            earnings += parseFloat(checkbox.dataset.earning);
-        }
-        saveCheckboxState(checkbox);
+  for (const checkbox of feedDogsCheckboxes) {
+    if (checkbox.checked) {
+      earnings += parseFloat(checkbox.dataset.earning);
     }
+    saveCheckboxState(checkbox);
+  }
 
-    totalEarnings.textContent = earnings.toFixed(2);
-    localStorage.setItem('choreChartEarnings', earnings.toFixed(2));
+  totalEarnings.textContent = earnings.toFixed(2);
 }
 
 function loadCheckboxState() {
-    const checkboxState = JSON.parse(localStorage.getItem('checkboxState')) || {};
+  database.ref('checkboxState/').once('value', (snapshot) => {
+    const checkboxState = snapshot.val() || {};
     for (const checkboxId in checkboxState) {
-        const checkbox = document.getElementById(checkboxId);
-        if (checkbox) {
-            checkbox.checked = checkboxState[checkboxId];
-        }
+      const checkbox = document.getElementById(checkboxId);
+      if (checkbox) {
+        checkbox.checked = checkboxState[checkboxId];
+      }
     }
+    updateEarnings();
+  });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    fillChoreTable();
-    const storedEarnings = localStorage.getItem('choreChartEarnings');
-    if (storedEarnings) {
-        totalEarnings.textContent = storedEarnings;
-    }
-    loadCheckboxState();
-    updateEarnings();
+  fillChoreTable();
+  loadCheckboxState();
 });
 
